5203cfca1bc2bb1d5709beda3095acb0f1ff31a9
dhanashree.metangale <dhanashree.metangale@ideabotkey.in>
2026-07-03 17:48:28 +0530

    event Website

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="mirror-shell">
+      <iframe
+        className="mirror-frame"
+        src="/100xengineers-clone.html"
+        title="100xEngineers clone"
+      />
+    </main>
+  );
+}
